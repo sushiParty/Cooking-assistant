82e17fae8e405e4808d25a19a2aa8ceae7a5f60b
Srivatsa <p.psrivatsa@gmail.com>
2018-01-26 23:49:02 +0800

    Add & import firebase api key to main app

---
+++ src/fire.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyCrnGbSIMkD2h09fCZd43sAKcVsnrPODeM",
+    authDomain: "cooking-assistant-b3183.firebaseapp.com",
+    databaseURL: "https://cooking-assistant-b3183.firebaseio.com",
+    projectId: "cooking-assistant-b3183",
+    storageBucket: "cooking-assistant-b3183.appspot.com",
+    messagingSenderId: "992162769843"
+  };
+  var fire = firebase.initializeApp(config);
+  export default fire;
