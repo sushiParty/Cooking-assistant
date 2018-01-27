3d994937c03e65340bb7a0243878476086bad26c
Kai Yu Quek <kaiyu92@hotmail.com>
2018-01-27 18:18:53 +0800

    Login Routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Routes from './routes';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
 registerServiceWorker();
