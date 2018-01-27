c9ef0c6d7c8285b0c0f4788b068d186b8b2b1bb6
Kaiser Tan <kaisertan@Kaisers-MacBook-Pro.local>
2018-01-27 23:25:25 +0800

    recipeView added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import fire from './fire';
-import CAwithTimer from './components/CookAsst/CAwithTimer';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CAwithTimer />
       </div>
     );
   }
