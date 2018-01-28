cb1895d7d5123799fc8da69adbf2c45a89cbe09c
Shi Tian <shitian007@hotmail.com>
2018-01-28 08:24:28 +0800

    refactor navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,66 @@
 import React, { Component } from 'react';
 import portrait from '../static/img/portrait.jpg';
-import { Menu } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
+
+import { Sidebar, Segment, Button, Icon, Header, Menu } from 'semantic-ui-react';
+import './navbar.css';
+
+import Main from './main/Main.jsx';
+import SearchBar from './SearchBar.js';
+
 
 class Navbar extends Component {
+  state = {
+    visible: true,
+    activeItem: 'home'
+  };
 
-  state = { activeItem: 'home' }
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  
   render() {
-    const { activeItem } = this.state
+    const { visible } = this.state;
+    const { activeItem } = this.state;
 
     return (
-      <div className = "Navbar" >
-        <Menu pointing secondary vertical>
-          <Menu.Header>
-            <img src={portrait} height= '100px' />
-            <h4> John Francis </h4>
-            </Menu.Header>
-            <br/>
-            <br/>
-          <Link to="/home">
-            <Menu.Item name='My Recipes' active={activeItem === 'My Recipes' } onClick={this.handleItemClick}> My Recipes </Menu.Item>
-          </Link>
+      <div className="navbar-wrapper">
+	<div>
+	  <div className="navbar-button">
+            <Button onClick={this.toggleVisibility}>≅Barz</Button>
+	  </div>
+	  <div className="top-search-bar">
+	    <SearchBar/>
+	  </div>
+	</div>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='slide out' width='thin' visible={visible} vertical inverted>
+	    <Link to="/home">
+              <Menu.Item name='My Recipes' active={activeItem === 'My Recipes' } onClick={this.handleItemClick}> My Recipes </Menu.Item>
+            </Link>
 
-          <Link to="/">
-          <Menu.Item name='Saved Recipes' active={activeItem === 'Saved Recipes'} onClick={this.handleItemClick}>Saved Recipes</Menu.Item>
-          </Link>
+            <Link to="/">
+              <Menu.Item name='Saved Recipes' active={activeItem === 'Saved Recipes'} onClick={this.handleItemClick}>Saved Recipes</Menu.Item>
+            </Link>
 
-          <Link to="/">
-          <Menu.Item name='Search' active={activeItem === 'Search'} onClick={this.handleItemClick} >Search</Menu.Item>
-          </Link>
+            <Link to="/">
+              <Menu.Item name='Create New Recipes' active={activeItem === 'Create New Recipes'} onClick={this.handleItemClick}>Create New Recipes</Menu.Item>
+            </Link>
 
-          <Link to="/">
-          <Menu.Item name='Create New Recipes' active={activeItem === 'Create New Recipes'} onClick={this.handleItemClick}>Create New Recipes</Menu.Item>
-          </Link>
-
-          <Menu.Item name='Logout' active={activeItem === 'Logout'} onClick={this.handleItemClick} />
-
-        </Menu>
+            <Menu.Item name='Logout' active={activeItem === 'Logout'} onClick={this.handleItemClick} />
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+	      <Switch>
+		<Route path="/home" component={ Main }/>
+              // <Route path="/myrecipes" component={ Main }/>
+              // <Route path="/savedrecipes" component={ Main }/>
+	      </Switch>
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
-    )
+    );
   }
 }
 
